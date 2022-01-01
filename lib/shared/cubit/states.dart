63b1b414ba2015c3aec7af91d5b387e759fa2bcb
islamgharib <islamahmed1992@yahoo.com>
2022-01-01 12:23:21 +0200

    create database transactions cubit and states

---
+++ lib/shared/cubit/states.dart
@@ -0,0 +1,6 @@
+abstract class UsersStates{}
+class AppInitialState extends UsersStates{}
+class AppCreateDatabaseState extends UsersStates{}
+class AppGetDataFromDatabaseState extends UsersStates{}
+class AppInsertDataToDatabaseState extends UsersStates{}
+class AppGetDatabaseLoadingState extends UsersStates{}
